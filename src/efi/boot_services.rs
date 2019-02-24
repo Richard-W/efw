4050652aa0ebbe0fee013d867bf4c3a1fe09e94b
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2019-02-24 19:50:03 +0100

    Add BootServices::get_memory_map call

--- src/efi/boot_services.rs
+++ src/efi/boot_services.rs
@@ -31,6 +31,28 @@ impl BootServices {
         status_to_result(((*self.0).free_pages)(memory as _, num))
     }
 
+    /// Get the current memory map.
+    pub unsafe fn get_memory_map(&self) -> Result<MemoryMap> {
+        let mut buffer_size: usize = 0;
+        let mut map_key: usize = 0;
+        let mut desc_size: usize = 0;
+        let mut desc_ver: u32 = 0;
+        ((*self.0).get_memory_map)(&mut buffer_size as _, 0 as _, &mut map_key as _, &mut desc_size as _, &mut desc_ver as _);
+
+        let mut buffer = Vec::new();
+        buffer.resize(buffer_size, 0);
+
+        status_to_result(((*self.0).get_memory_map)(
+            &mut buffer_size as _,
+            &mut buffer.as_mut_slice()[0] as *mut u8 as *mut _,
+            &mut map_key as _,
+            &mut desc_size as _,
+            &mut desc_ver as _)
+        )?;
+
+        Ok(MemoryMap::new(buffer, desc_size, map_key))
+    }
+
     /// Get an array of handles that support a specific protocol.
     pub unsafe fn locate_handle(&self, search_type: bits::LocateSearchType, protocol: *mut bits::Guid, search_key: *mut core::ffi::c_void) -> Result<Vec<Handle>> {
         // Find out needed buffer size.
@@ -55,5 +77,67 @@ impl BootServices {
         let mut interface: *mut core::ffi::c_void = 0 as _;
         status_to_result(((*self.0).handle_protocol)(handle.value() as bits::Handle, protocol, &mut interface as _))?;
         Ok(interface)
+    }
+}
+
+/// UEFI Memory Map container.
+pub struct MemoryMap {
+    pub(crate) buffer: Vec<u8>,
+    pub(crate) desc_size: usize,
+    map_key: usize,
+}
+
+impl MemoryMap {
+    pub(crate) fn new(buffer: Vec<u8>, desc_size: usize, map_key: usize) -> Self {
+        MemoryMap {
+            buffer: buffer,
+            desc_size: desc_size,
+            map_key: map_key,
+        }
+    }
+
+    /// Key of the memory map.
+    pub fn key(&self) -> usize {
+        self.map_key
+    }
+
+    /// Iterate through the map entries.
+    pub fn iter(&mut self) -> MemoryMapIterator {
+        MemoryMapIterator::new(self)
+    }
+}
+
+/// Iterator type for the `MemoryMap` struct.
+pub struct MemoryMapIterator<'a> {
+    mmap: &'a mut MemoryMap,
+    position: usize,
+}
+
+impl<'a> MemoryMapIterator<'a> {
+    pub(crate) fn new(mmap: &'a mut MemoryMap) -> Self {
+        MemoryMapIterator {
+            mmap: mmap,
+            position: 0,
+        }
+    }
+}
+
+impl<'a> core::iter::Iterator for MemoryMapIterator<'a> {
+    type Item = &'a mut bits::MemoryDescriptor;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item_offset = self.mmap.desc_size * self.position;
+        let item_end = item_offset + self.mmap.desc_size;
+        if item_end >= self.mmap.buffer.len() {
+            None
+        }
+        else {
+            let ptr: *mut u8 = &mut self.mmap.buffer.as_mut_slice()[0] as _;
+            unsafe {
+                let desc_ptr = ptr.offset(item_offset as isize) as *mut bits::MemoryDescriptor;
+                self.position += 1;
+                Some(&mut *desc_ptr)
+            }
+        }
     }
 }
--- test-runner/src/main.rs
+++ test-runner/src/main.rs
@@ -13,4 +13,20 @@ fn efw_main() {
 
     let text_output_instances = efw::efi::protocols::SimpleTextOutput::find_instances().unwrap();
     println!("Text Output Instances found: {:?}", text_output_instances.len());
+
+    let mut memory_map = unsafe {
+        efw::efi::SystemTable::get()
+            .boot_services()
+            .get_memory_map()
+            .unwrap()
+    };
+
+    println!("Memory map");
+    for desc in memory_map.iter() {
+        println!("  Type:       {:#x}", desc.type_);
+        println!("  Phys Start: {:#x}", desc.physical_start);
+        println!("  Virt Start: {:#x}", desc.virtual_start);
+        println!("  Num Pages:  {:#x}", desc.number_of_pages);
+        println!();
+    }
 }
