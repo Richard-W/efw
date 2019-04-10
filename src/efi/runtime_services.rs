e945c3e0b8df135b095eecc81e4b55e73cbd9016
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2019-04-10 17:43:01 +0200

    Document runtime services function

--- src/efi/runtime_services.rs
+++ src/efi/runtime_services.rs
@@ -7,6 +7,8 @@ impl RuntimeServices {
         RuntimeServices(runtime_services)
     }
 
+    /// Announces virtual address mappings to EFI components so they can continue to work
+    /// after page tables have been modified.
     pub unsafe fn set_virtual_address_map(&self, memory_map: &mut MemoryMap) -> Result<()> {
         let length = memory_map.buffer.len() * memory_map.desc_size;
         status_to_result(((*self.0).set_virtual_address_map)(length, memory_map.desc_size, memory_map.desc_ver, &mut memory_map.buffer[0] as *mut _ as _))
