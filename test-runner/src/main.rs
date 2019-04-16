e5fca5148b8cf2fab481c017870eacb3f239c7dc
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2019-04-16 11:13:06 +0200

    Count memory map in test-runner instead of dumping it

--- test-runner/src/main.rs
+++ test-runner/src/main.rs
@@ -14,17 +14,14 @@ fn efw_main() {
     let text_output_instances = efw::efi::protocols::SimpleTextOutput::find_instances().unwrap();
     println!("Text Output Instances found: {:?}", text_output_instances.len());
 
-    let mut memory_map = unsafe {
+    let memory_map = unsafe {
         efw::efi::SystemTable::get()
             .boot_services()
             .get_memory_map()
             .unwrap()
     };
 
-    println!("Memory map");
-    for desc in memory_map.iter_mut() {
-        println!("Mutable entry of type {:#x}", desc.r#type);
-    }
+    println!("Memory map ({} entries)", memory_map.iter().count());
 
     let mut graphics_output_instances = efw::efi::protocols::GraphicsOutput::find_instances().unwrap();
     println!("Found {} graphics output protocols", graphics_output_instances.len());
